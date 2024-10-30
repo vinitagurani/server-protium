44f264c1500478f754775d337043034812534ce0
Vinita Gurnani <136057948+vinitagurani@users.noreply.github.com>
2024-10-30 13:20:18 +0530

    modified cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin: 'https://tasks-assign.netlify.app/', // Change this to your frontend URL
+  origin: 'https://tasks-assign.netlify.app', // Change this to your frontend URL
 }));
 
 app.use(bodyParser.json());
